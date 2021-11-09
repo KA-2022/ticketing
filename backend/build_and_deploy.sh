a03fed7c9040359586a9ee75cdf22d346af36579
slippedandmissed <gbt505@hotmail.com>
2021-11-09 19:58:01 +0000

    Un-gitignore'd package-lock.json on frontend Removed build tools because I set up CD

--- backend/build_and_deploy.sh
+++
@@ -1,2 +0,0 @@
-docker build . --tag europe-west2-docker.pkg.dev/ticketing-dev-331618/ticketing/ticketing-backend:latest
-docker push europe-west2-docker.pkg.dev/ticketing-dev-331618/ticketing/ticketing-backend:latest
