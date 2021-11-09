39de26a38ebde7ec51298847b80c6249e5077ac9
slippedandmissed <gbt505@hotmail.com>
2021-11-09 23:45:17 +0000

    QR codes are now generated from a GET parameter

--- backend/api/ticket.js
+++ backend/api/ticket.js
@@ -4,8 +4,8 @@ const express = require("express");
 const { generateQRCode } = require("../tools");
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-    const data = "Lorem ipsum";
+router.get("/qr", async (req, res) => {
+    const data = Buffer.from(req.query.d, "base64").toString("utf-8");
     const buffer = await generateQRCode(data);
     res.header("Content-Type", "image/png");
     res.send(buffer);
